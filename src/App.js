1ad5a3fb16c2202568ab2af0501c628272e1b1c6
Seba1705 <sebaaguirre2012@gmail.com>
2019-09-17 11:14:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'; 
+import Login from './components/Login'; 
+import Main from './components/Main'; 
+import Register from './components/Register'; 
+
+export default class App extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className='container-fluid'>
+                    <Header/>
+                    <Switch>
+                        <Route path='/login' render={() => <Login/>} />
+                        <Route path='/main' />
+                        <Route path='/register' />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <div className="nav-wrapper blue darken-4">
+                <a href="#" className="brand-logo center">Pwa Social</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
